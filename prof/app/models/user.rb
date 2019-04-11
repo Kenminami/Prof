f4c84cd1bcbb10948e2caf0ea5b26479e12e335a
南健士郎 <nsr.minami0328@gmail.com>
2019-04-11 17:53:29 +0900

    アバター実装
    アバター登録
    paperclip
    imagemagick install

--- prof/app/models/user.rb
+++ prof/app/models/user.rb
@@ -9,6 +9,8 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable,
          :validatable, :confirmable,
          authentication_keys: [:email, :group_key]
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>"}
+  validates_attachment_content_type :avatar, content_type: ["image/jpg","image/jpeg","image/png"]
 
   #association
   belongs_to :group
